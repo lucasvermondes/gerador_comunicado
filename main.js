78ed2172a7ed6f7b64bec66b2370e928adbc833a
Lucas Vermondes <105724033+lucasvermondes@users.noreply.github.com>
2026-04-09 16:21:10 -0300

    Add icon path to application configuration

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow() {
     minWidth: 1100,
     minHeight: 700,
     autoHideMenuBar: true,
+    icon: path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
